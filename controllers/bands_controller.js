341c6861fa6943eb69274ab0cd395a87a29e8cde
Jam19th <jam19th@gmail.com>
2023-08-26 11:05:18 -0700

    Activity (sqldm-5-sequelize-helper-methods) Commit

---
+++ controllers/bands_controller.js
@@ -0,0 +1,88 @@
+//Dependencies
+const bands = require('express').Router();
+const db = require('../models');
+const { Band } = db;
+
+// Find all bands in the database
+bands.get('/', async (req, res) => {
+    try {
+        const foundBands = await Band.findAll()
+        res.status(200).json({ foundBands });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Something went wrong', error: error.message });
+    }
+})
+
+// Find one band by id
+bands.get('/:id', async (req, res) => {
+    try {
+        const foundBand = await Band.findOne({
+            where: { band_id: req.params.id }
+        })
+        res.status(200).json({ foundBand });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Something went wrong', error: error.message });
+    }
+})
+
+// Create a new band
+bands.post('/', async (req, res) => {
+    try {
+        const newBand = await Band.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new band',
+            data: newBand
+        })
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Something went wrong', error: error.message });
+    }
+})
+
+// Update a band by id
+bands.put('/:id', async (req, res) => {
+    try {
+        const updatedBands = await Band.update(req.body, {
+            where: { band_id: req.params.id },
+            returning: true
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedBands} band(s)`,
+            data: updatedBands
+        })
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Something went wrong', error: error.message });
+    }
+})
+
+// Delete a band by id
+bands.delete('/:id', async (req, res) => {
+    try {
+        const deletedBands = await Band.destroy({
+            where: { band_id: req.params.id }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedBands} band(s)`,
+            data: deletedBands
+        })
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Something went wrong', error: error.message });
+    }
+})
+
+// 404 error
+// bands.get('*', (req, res) => {
+//     try {
+//         res.status(404).json({ message: 'Page not found' });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ message: 'Something went wrong', error: error.message });
+//     }
+// })
+
+// Export the bands controller
+module.exports = bands;
--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const express = require('express')
 const app = express()
 const { Sequelize } = require('sequelize')
 
+// CONTROLLERS
+const bandsController = require('./controllers/bands_controller.js')
+
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use('/bands', bandsController)
 
 // ROOT
 app.get('/', (req, res) => {

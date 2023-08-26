b3477333f7fbfa2fc258ef18b072176f9d90069c
Jam19th <jam19th@gmail.com>
2023-08-25 23:27:58 -0700

    Activity (sqldm-4-sequelize-cli-and-migrations) Commit

--- server.js
+++ server.js
@@ -8,23 +8,6 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-//Sequelize connection
-const sequelize = new Sequelize({
-    storage: process.env.PG_URI,
-    dialect: 'postgres',
-    username: 'postgres',
-    password: 'chobits',
-});
-
-// Test connection
-try {
-    console.log(process.env.PG_URI);
-    sequelize.authenticate();
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`);
-} catch (error) {
-    console.error(`Unable to connect to PG: ${error}`);
-}
-
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
